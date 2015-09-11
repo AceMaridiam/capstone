f53b5272b5ffd909949495cc262e5404730835bd
AceMaridiam <andermation@gmail.com>
2015-09-10 19:39:57 -0700

    Setup first view and created templates/blog/post_list.html

--- capstone/blog/models.py
+++ capstone/blog/models.py
@@ -8,7 +8,8 @@ class Post(models.Model):
 	title          = models.CharField(max_length=255)
 	text           = models.TextField()
 	created_date   = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField(blank=True, null=True) 
+	published_date = models.DateTimeField(blank=True, null=True)
+	# image          = models.ImageField(upload_to="", blank=True, null=True)
 
 	def publish(self):
 		self.published_date = timezone.now()
