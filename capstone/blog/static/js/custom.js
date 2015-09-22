18b53094785ef9d1c0c9b78abc69223d87a7d262
AceMaridiam <andermation@gmail.com>
2015-09-22 01:06:58 -0700

    -v

--- capstone/blog/static/js/custom.js
+++ capstone/blog/static/js/custom.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	function isValidEmail(email)
+	{
+    	return /^[a-z0-9]+([-._][a-z0-9]+)*@([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,4}$/.test(email)
+        && /^(?=.{1,64}@.{4,64}$)(?=.{6,100}$).*/.test(email);
+	}
+
 	// modal
 	$('.pop').on('click', function() {
 		$('.imagepreview').attr('src', $(this).find('img').attr('src'));
@@ -31,10 +37,54 @@ $(document).ready(function() {
 		$(this).animate({left: "-200px"}, {duration: 500});
 	})
 
-	// var icon = $('.icon');
-	// for(i=0; i<icon.length; i++) {
-	// 	icon.classList.add('me');
+	// email validation
+	// if($("input#email-address").getVerimailStatus() < 0){
+	//     alert('Nope');
+	// }else{
+	//     alert('Yipee!!');
 	// }
+
+	// $("input").verimail({
+	//     messageElement: "p#status-message"
+	// });
+
+	$('.subscribe .btn').click(function() {
+		var email = $('.subscribe input').val();
+		// console.log(isValidEmail(email));
+		if (isValidEmail(email)) {
+			console.log('Yipee!');
+		}
+		else {
+			console.log('Nope');
+		}
+	})
+
+
+
+	// hover info boxes for tool icons
+	$(function () {
+	  $('[data-toggle="tooltip"]').tooltip()
+	})
+	// hover info boxes text content
+	$(".icon").mouseenter(function(){
+		if($(this).hasClass('icon-technical-pen')) {
+	    	$(this).attr('data-original-title', "Pen - 0.5 & 0.7 Micron");
+	    }
+	    if($(this).hasClass('icon-compass')) {
+	    	$(this).attr('data-original-title', "Compass - An antique used to get highly accurate circles");
+	    }
+	    if($(this).hasClass('icon-pen')) {
+	    	$(this).attr('data-original-title', "Dipping Pen - used with hand mixed ink(s)");
+	    }
+	    if($(this).hasClass('icon-lightbox')) {
+	    	$(this).attr('data-original-title', "Lightbox - to help draw and transer shapes");
+	    }
+	    if($(this).hasClass('icon-airbrush')) {
+	    	$(this).attr('data-original-title', "Airbrush - for smooth and elegant color transitions");
+	    }
+	});
+
+	
 	
 	
 
